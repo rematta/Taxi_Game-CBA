380cd15d3de1a01104e64c2713ccb827ae080c42
Adhi Ravishankar <adhiravishankar@gmail.com>
2018-12-08 18:58:38 -0500

    added classifiers

--- classifiers.py
+++ classifiers.py
@@ -1,2 +1,34 @@
-def classifier(state):
-    print(state)
+import numpy
+from scipy.spatial import cKDTree
+
+from sklearn.neighbors import NearestNeighbors
+from sklearn.svm import SVC
+
+
+def classifier(svm: SVC, state):
+    taxirow, taxicol, passloc, destidx = state
+    feature = [taxirow, taxicol, passloc, destidx]
+    c = svm.decision_function(feature)
+    a_p = svm.predict(feature)
+    return a_p, c, None
+
+
+def update_classifier(states, actions):
+    svm = SVC()
+    svm.fit(states, actions)
+    return svm
+
+
+def nearest_neighbor(nn: NearestNeighbors, state):
+    return nn.kneighbors(state, 1)
+
+
+def update_threshold(states: list):
+    kdt = cKDTree(states)
+    dists, neighs = kdt.query(states, 1)
+    avg_dist = numpy.mean(dists[:, 1:], axis=1)
+    return 0, avg_dist
+
+def process_state(state):
+    taxirow, taxicol, passloc, destidx = state
+    return taxirow, taxicol, passloc, destidx
--- taxi-cba.py
+++ taxi-cba.py
@@ -5,8 +5,8 @@
 import gym
 import numpy as np
 from IPython.display import clear_output
-
-from classifiers import classifier
+from sklearn.neighbors import NearestNeighbors
+from classifiers import classifier, update_classifier, process_state, nearest_neighbor, update_threshold
 
 env = gym.make("Taxi-v2").env
 
@@ -17,15 +17,18 @@ t_dist_gamma = 2
 
 # For plotting metrics
 all_epochs = []
+svm = None
+q_table = np.zeros([env.observation_space.n, env.action_space.n])
+nn = NearestNeighbors()
 
 for i in range(1, 100001):
     state = env.reset()
     epochs = 0
     done = False
-    T = []
+    states = []
+    actions = []
     t_conf = float("inf")
     t_dist = 0
-
 
     while not done:
         # 1. get sensor data step (i.e. the current state.  this is different for continuous actions)
@@ -34,11 +37,11 @@ for i in range(1, 100001):
         while pres != "y" and pres != "n":
             pres = input("Do you want to correct the last action taken? [y/n]: ")
         
-        if pres == "n":     # 3. execute the Confident Execution step
+        if pres == "n" or svm is not None:     # 3. execute the Confident Execution step
             # 5. Put current state into classifier to get a_p, c, and db
             # 6. Get nearest neighbor for state
-            a_p, c, db = classifier(env.decode(state))
-            d = nearest_neighbor(env.decode(state))
+            a_p, c, db = classifier(svm, env.decode(state))
+            d = nearest_neighbor(nn, env.decode(state))
 
             if c > t_conf and d < t_dist:
                 state, reward, done, info = env.step(a_p)
@@ -46,17 +49,20 @@ for i in range(1, 100001):
                 pres = -1
                 while pres not in [0, 1, 2, 3, 4, 5]:
                     pres = input("Expert: enter the next action I should take [0-5]: ")
-                T.append((state, pres))
-                update_classifier(T)
+                states.append(process_state(env.decode(state)))
+                actions.append(pres)
+                svm = update_classifier(states, actions)
                 t_conf, t_dist = update_threshhold()
 
                 state, reward, done, info = env.step(pres)
-        else:               # 4. execute the Corrective Demonstration step
+        else:   # 4. execute the Corrective Demonstration step
             pres = -1
+            env.render()
             while pres not in [0, 1, 2, 3, 4, 5]:
-                pres = input("Expert: enter the next action I should take [0-5]: ")
-            T.append((state, pres))
-            update_classifier(T)
+                pres = int(input("Expert: enter the next action I should take [0-5]: "))
+            states.append(state)
+            actions.append(pres)
+            svm = update_classifier(states, actions)
             t_conf, t_dist = update_threshhold()            
 
         
